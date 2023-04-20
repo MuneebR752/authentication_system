7341b0d9a774aa666dd686d4bc486c2eaf51e9a9
MuneeburRehman <muneeburrehman440@gmail.com>
2023-04-20 23:17:47 +0500

    Added log out functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 export default function Navbar() {
+  let navigate = useNavigate();
   return (
     <div className="topnav">
       <NavLink to="/">Dashboard</NavLink>
       <NavLink to="/about">About</NavLink>
+      <div
+        onClick={() => {
+          localStorage.removeItem("token");
+          navigate("/login");
+        }}
+      >
+        Log out
+      </div>
     </div>
   );
 }
